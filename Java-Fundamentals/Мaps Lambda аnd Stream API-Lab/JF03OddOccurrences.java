9f17dc16c520a1a5ead1be71d05115625546551d
Ivan Marinov <106252758+vando1661@users.noreply.github.com>
2022-10-27 09:39:48 +0100

    Add files via upload

---
+++ Java-Fundamentals/Мaps Lambda аnd Stream API-Lab/JF03OddOccurrences.java
@@ -0,0 +1,28 @@
+package mapsLambdaAndStreamAPILab;
+
+import java.util.*;
+
+public class JF03OddOccurrences {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        String [] lineInput = scanner.nextLine().split(" ");
+        Map<String, Integer> wordsCountMap = new LinkedHashMap<>();
+        for (int i = 0; i <lineInput.length ; i++) {
+            String currentWord = lineInput[i].toLowerCase();
+            Integer count = wordsCountMap.get(currentWord);
+            if(wordsCountMap.containsKey(currentWord)){
+                wordsCountMap.put(currentWord,count + 1);
+            }else {
+                wordsCountMap.put(currentWord, 1);
+            }
+
+        }
+        List<String> oodWords = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : wordsCountMap.entrySet()) {
+            if(entry.getValue() % 2 == 1 ){
+                oodWords.add(entry.getKey());
+            }
+        }
+        System.out.println(String.join(", ",oodWords));
+    }
+}
