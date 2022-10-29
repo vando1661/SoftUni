8b6650f14d073887f81daf153a2a3cd7b17022f0
Ivan Marinov <106252758+vando1661@users.noreply.github.com>
2022-10-29 23:02:02 +0100

    Add files via upload

---
+++ Java-Fundamentals/Regular Expressions-Lab/JF03MatchDates.java
@@ -0,0 +1,20 @@
+package regularExpressionsLab;
+
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class JF03MatchDates {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+        String input =scanner.nextLine();
+        Pattern pattern = Pattern.compile("\\b(?<day>\\d{2})([-.\\/]{1})(?<month>[A-Z][a-z]{2})(?:\\2)(?<year>\\d{4})\\b");
+        Matcher matcher = pattern.matcher(input);
+        while (matcher.find()){
+            String day = matcher.group("day");
+            String month = matcher.group("month");
+            String year = matcher.group("year");
+            System.out.printf("Day: %s, Month: %s, Year: %s%n",day,month,year);
+        }
+    }
+}
