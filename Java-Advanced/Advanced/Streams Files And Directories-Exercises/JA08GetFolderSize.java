2006913993e418b029347cd1a083ad6b72020976
Ivan Marinov <vando1661@gmail.com>
2023-01-17 09:23:32 +0000

    Create JA08GetFolderSize.java

---
+++ Java-Advanced/Advanced/Streams Files And Directories-Exercises/JA08GetFolderSize.java
@@ -0,0 +1,29 @@
+package _8StreamsFilesAndDirectories_Exercises;
+
+import java.io.File;
+import java.util.ArrayDeque;
+
+public class JA08GetFolderSize {
+    public static void main(String[] args) {
+        File folder = new File("resources");
+        ArrayDeque<File> directories = new ArrayDeque<>();
+        directories.offer(folder);
+
+        int sumOfBytes = 0;
+
+        while (!directories.isEmpty()) {
+            File current = directories.poll();
+            File[] files = current.listFiles();
+            for (File file : files) {
+                if (file.isDirectory()) {
+                    directories.offer(file);
+                } else {
+                    sumOfBytes += file.length();
+                }
+            }
+        }
+
+        System.out.println(sumOfBytes);
+    }
+}
+
