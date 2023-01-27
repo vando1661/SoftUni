9848cf4e4a671b48fdc7fb66270ddc6108c22058
Ivan Marinov <vando1661@gmail.com>
2023-01-27 00:39:04 +0000

    Create Main.java

---
+++ Java-Advanced/Advanced/Generics-Lab/JA01JarOfT/Main.java
@@ -0,0 +1,13 @@
+package Java_Advance._13GenericsLab.JA01JarOfT;public class Main {
+
+        public static void main(String[] args) {
+
+            Jar<String> stringJan = new Jar<>();
+            stringJan.add("Ivan");
+            stringJan.add("Pesho");
+
+
+        }
+
+}
+
