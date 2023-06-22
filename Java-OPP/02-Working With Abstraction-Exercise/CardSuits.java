bbc6f80bec945069b2594331e7b15b2ef1d1fd01
Ivan Marinov <vando1661@gmail.com>
2023-06-22 23:16:35 +0100

    Create CardSuits.java

---
+++ Java-OPP/02-Working With Abstraction-Exercise/CardSuits.java
@@ -0,0 +1,14 @@
+package Java_OOP._02WorkingWithAbstraction_Exercise;
+
+public enum CardSuits {
+    CLUBS(0), DIAMONDS(13), HEARTS(26), SPADES(39);
+    private int suitPower;
+
+    CardSuits(int suitPower) {
+        this.suitPower = suitPower;
+    }
+
+    public int getSuitPower() {
+        return suitPower;
+    }
+}
