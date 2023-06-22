1650a20ce349902cbb816211eb46c8cfb36c962d
Ivan Marinov <vando1661@gmail.com>
2023-06-23 00:17:34 +0100

    Create Team.java

---
+++ Java-OPP/03-Encapsulation-Lab/Team.java
@@ -0,0 +1,38 @@
+package Java_OOP._03EncapsulationLab;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Team {
+    private String name;
+    private List<Person> firstTeam;
+    private List<Person> reserveTeam;
+
+    public Team(String name) {
+        this.setName(name);
+        this.firstTeam = new ArrayList<>();
+        this.reserveTeam = new ArrayList<>();
+    }
+
+    public List<Person> getFirstTeam() {
+        return Collections.unmodifiableList(firstTeam);
+    }
+
+    public List<Person> getReserveTeam() {
+        return Collections.unmodifiableList(reserveTeam);
+    }
+
+    public void addPlayer(Person person) {
+        if (person.getAge() < 40) {
+            firstTeam.add(person);
+        } else {
+            reserveTeam.add(person);
+        }
+
+    }
+
+    private void setName(String name) {
+        this.name = name;
+    }
+}
