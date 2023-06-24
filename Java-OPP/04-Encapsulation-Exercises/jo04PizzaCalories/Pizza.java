0a5fa903474ff3ec3eea6d2149ca88e827d2ae9f
Ivan Marinov <vando1661@gmail.com>
2023-06-25 00:32:56 +0100

    Create Pizza.java

---
+++ Java-OPP/04-Encapsulation-Exercises/jo04PizzaCalories/Pizza.java
@@ -0,0 +1,46 @@
+package Java_OOP._04EncapsulationExercises.jo04PizzaCalories;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Pizza {
+    private String name;
+    private Dough dough;
+    private List<Topping> toppings;
+
+    public Pizza(String name, int countOfToppings) {
+         setName(name);
+         setToppings(countOfToppings);
+    }
+
+    private void setToppings(int countToppings) {
+        if (countToppings < 0 || countToppings > 10) {
+            throw new IllegalArgumentException("Number of toppings should be in range [0..10]");
+        }
+        this.toppings = new ArrayList<>();
+    }
+
+    private void setName(String name) {
+        if (name.trim().isEmpty() || name.length() > 15) {
+            throw new IllegalArgumentException("Pizza name should be between 1 and 15 symbols.");
+        }
+        this.name = name;
+    }
+
+    public void setDough(Dough dough) {
+        this.dough = dough;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void addTopping(Topping topping) {
+        toppings.add(topping);
+
+    }
+
+    public double getOverallCalories() {
+return dough.calculateCalories() + toppings.stream().mapToDouble(Topping::calculateCalories).sum();
+    }
+}
