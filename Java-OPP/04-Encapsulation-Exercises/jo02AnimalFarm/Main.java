1b90f39b22bf59efbfcff2a5dff3db4b0a8304a3
Ivan Marinov <vando1661@gmail.com>
2023-06-25 00:32:47 +0100

    Create Main.java

---
+++ Java-OPP/04-Encapsulation-Exercises/jo02AnimalFarm/Main.java
@@ -0,0 +1,17 @@
+package Java_OOP._04EncapsulationExercises.jo02AnimalFarm;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String name = scanner.nextLine();
+        int age = Integer.parseInt(scanner.nextLine());
+        try {
+            Chicken chicken = new Chicken(name,age);
+            System.out.println(chicken);
+        }catch (IllegalArgumentException exception){
+            System.out.println(exception.getMessage());
+        }
+    }
+}
