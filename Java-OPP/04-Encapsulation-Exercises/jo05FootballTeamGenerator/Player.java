04632596950c16c4535d90f718fff19ce704bce4
Ivan Marinov <vando1661@gmail.com>
2023-06-25 00:32:21 +0100

    Create Player.java

---
+++ Java-OPP/04-Encapsulation-Exercises/jo05FootballTeamGenerator/Player.java
@@ -0,0 +1,69 @@
+package Java_OOP._04EncapsulationExercises.jo05FootballTeamGenerator;
+
+public class Player {
+    private  String name;
+    private int endurance;
+    private int sprint;
+    private int dribble;
+    private int passing;
+    private int shooting;
+
+    public Player(String name, int endurance, int sprint, int dribble, int passing, int shooting) {
+        setName(name);
+        setEndurance(endurance);
+        setSprint(sprint);
+        setDribble(dribble);
+        setPassing(passing);
+        setShooting(shooting);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    private void setName(String name) {
+        if(name.trim().isEmpty()){
+            throw new IllegalArgumentException("A name should not be empty.");
+        }
+        this.name = name;
+    }
+
+    private void setEndurance(int endurance) {
+        if(endurance < 0 || endurance > 100){
+            throw new IllegalArgumentException("Endurance should be between 0 and 100.");
+        }
+        this.endurance = endurance;
+    }
+
+    private void setSprint(int sprint) {
+        if(sprint < 0 || sprint > 100){
+            throw new IllegalArgumentException("Sprint should be between 0 and 100.");
+        }
+        this.sprint = sprint;
+    }
+
+    private void setDribble(int dribble) {
+        if(dribble < 0 || dribble > 100){
+            throw new IllegalArgumentException("Dribble should be between 0 and 100.");
+        }
+        this.dribble = dribble;
+    }
+
+    private void setPassing(int passing) {
+        if(passing < 0 || passing > 100){
+            throw new IllegalArgumentException("Passing should be between 0 and 100.");
+        }
+        this.passing = passing;
+    }
+
+    private void setShooting(int shooting) {
+        if(shooting < 0 || shooting > 100){
+            throw new IllegalArgumentException("Shooting should be between 0 and 100.");
+        }
+        this.shooting = shooting;
+    }
+    public double overallSkillLevel(){
+        return  (endurance + sprint + dribble + passing + shooting) / 5.0;
+
+    }
+}
