ba22f13f67a30f25282040dd54668911066876b4
Ivan Marinov <vando1661@gmail.com>
2023-06-27 00:04:03 +0100

    Create ColdBeverage.java

---
+++ Java-OPP/06-Inheritance-Exercises/restaurant/ColdBeverage.java
@@ -0,0 +1,9 @@
+package Java_OOP._06InheritanceExercises.restaurant;
+
+import java.math.BigDecimal;
+
+public class ColdBeverage extends Beverage {
+    public ColdBeverage(String name, BigDecimal price, double milliliters) {
+        super(name, price, milliliters);
+    }
+}
