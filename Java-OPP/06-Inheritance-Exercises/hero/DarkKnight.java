e229da16fcf2634254fd2a2627f915c62594b6f7
Ivan Marinov <vando1661@gmail.com>
2023-06-27 00:03:27 +0100

    Create DarkKnight.java

---
+++ Java-OPP/06-Inheritance-Exercises/hero/DarkKnight.java
@@ -0,0 +1,7 @@
+package Java_OOP._06InheritanceExercises.hero;
+
+public class DarkKnight extends Knight{
+    public DarkKnight(String username, int level) {
+        super(username, level);
+    }
+}
