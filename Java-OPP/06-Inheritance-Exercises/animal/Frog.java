6a6eb0b73448003b9f2d0cb47a66c4de707f6778
Ivan Marinov <vando1661@gmail.com>
2023-06-27 00:03:21 +0100

    Create Frog.java

---
+++ Java-OPP/06-Inheritance-Exercises/animal/Frog.java
@@ -0,0 +1,11 @@
+package Java_OOP._06InheritanceExercises.animal;
+
+public class Frog extends Animal{
+
+    public Frog(String name, int age, String gender) {
+        super(name, age, gender);
+    }
+    public String produceSound(){
+        return "Ribbit";
+    }
+}
