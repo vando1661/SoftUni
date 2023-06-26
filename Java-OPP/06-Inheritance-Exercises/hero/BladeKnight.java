f4ba14136e9ed842681468e93a97d95f86e610dd
Ivan Marinov <vando1661@gmail.com>
2023-06-27 00:03:26 +0100

    Create BladeKnight.java

---
+++ Java-OPP/06-Inheritance-Exercises/hero/BladeKnight.java
@@ -0,0 +1,8 @@
+package Java_OOP._06InheritanceExercises.hero;
+
+public class BladeKnight extends DarkKnight{
+
+    public BladeKnight(String username, int level) {
+        super(username, level);
+    }
+}
