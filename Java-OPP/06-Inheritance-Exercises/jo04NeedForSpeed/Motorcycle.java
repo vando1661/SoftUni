4b143166c3f49a5d2390ee2ae5b62a953f842699
Ivan Marinov <vando1661@gmail.com>
2023-06-27 00:03:54 +0100

    Create Motorcycle.java

---
+++ Java-OPP/06-Inheritance-Exercises/jo04NeedForSpeed/Motorcycle.java
@@ -0,0 +1,7 @@
+package Java_OOP._06InheritanceExercises.jo04NeedForSpeed;
+
+public class Motorcycle extends Vehicle{
+    public Motorcycle(double fuel, int horsePower) {
+        super(fuel, horsePower);
+    }
+}
