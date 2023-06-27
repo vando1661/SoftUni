b43ab03e4b7da8b3786e99b4f647bdb56019dbcc
Ivan Marinov <vando1661@gmail.com>
2023-06-27 22:25:07 +0100

    Create Audi.java

---
+++ Java-OPP/07-Interfaces-And-Abstraction-Lab/jo01CarShop/Audi.java
@@ -0,0 +1,33 @@
+package Java_OOP._07InterfacesAndAbstractionLab.jo01CarShop;
+
+public class Audi extends CarImpl implements Rentable{
+    private Integer minRentDay;
+
+    private Double pricePerDay;
+
+    public Audi(String model, String color, Integer horsePower, String countryProduced,Integer minRentDay,Double pricePerDay) {
+        super(model,color,horsePower,countryProduced);
+        this.minRentDay = minRentDay;
+        this.pricePerDay = pricePerDay;
+
+    }
+
+    @Override
+    public Integer getMinRentDay() {
+        return minRentDay;
+    }
+
+    @Override
+    public Double getPricePerDay() {
+        return pricePerDay;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append(super.toString());
+        sb.append(System.lineSeparator());
+        sb.append(String.format("Minimum rental period of %d days. Price per day %f",getMinRentDay(),getPricePerDay()));
+        return sb.toString();
+    }
+}
