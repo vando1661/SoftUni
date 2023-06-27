379b0ece23d144d45a1742e507e0bc52c35023be
Ivan Marinov <vando1661@gmail.com>
2023-06-27 22:25:37 +0100

    Create Main.java

---
+++ Java-OPP/07-Interfaces-And-Abstraction-Lab/jo06Ferrari/Main.java
@@ -0,0 +1,12 @@
+package Java_OOP._07InterfacesAndAbstractionLab.jo06Ferrari;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String driverName = scanner.nextLine();
+        Car car = new Ferrari(driverName);
+        System.out.println(car);
+    }
+}
