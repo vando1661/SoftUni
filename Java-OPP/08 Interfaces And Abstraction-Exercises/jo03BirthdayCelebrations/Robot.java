5f31a7873da7e442bd3ade399780c1a2b4f62a98
Ivan Marinov <vando1661@gmail.com>
2023-06-29 22:31:58 +0100

    Create Robot.java

---
+++ Java-OPP/08 Interfaces And Abstraction-Exercises/jo03BirthdayCelebrations/Robot.java
@@ -0,0 +1,18 @@
+package Java_OOP._08InterfacesAndAbstractionExercises.jo03BirthdayCelebrations;
+
+public class Robot implements Identifiable  {
+
+    private String id;
+    private String model;
+    public Robot(String id, String model){
+        this.id = id;
+        this.model = model;
+    }
+    public String getId(){
+        return id;
+    }
+    public String getModel(){
+        return model;
+    }
+}
+
