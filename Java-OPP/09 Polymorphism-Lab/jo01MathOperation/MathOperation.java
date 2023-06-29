da8e24527f6b141b99437f412a57bd3b2e8efb74
Ivan Marinov <vando1661@gmail.com>
2023-06-30 00:54:52 +0100

    Create MathOperation.java

---
+++ Java-OPP/09 Polymorphism-Lab/jo01MathOperation/MathOperation.java
@@ -0,0 +1,14 @@
+package Java_OOP._09PolymorphismLab.jo01MathOperation;
+
+public class MathOperation {
+
+    public static int add(int a, int b){
+       return  a + b;
+    }
+    public static int add(int a, int b, int c){
+        return a + b + c;
+    }
+    public static int add(int a, int b, int c, int d){
+        return a + b + c + d;
+    }
+}
