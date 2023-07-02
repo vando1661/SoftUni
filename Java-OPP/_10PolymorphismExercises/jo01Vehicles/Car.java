c6150929c2f0476e99a30e9714bac4fea8bcf420
Ivan Marinov <vando1661@gmail.com>
2023-07-02 16:25:27 +0100

    Create Car.java

---
+++ Java-OPP/_10PolymorphismExercises/jo01Vehicles/Car.java
@@ -0,0 +1,10 @@
+package Java_OOP._10PolymorphismExercises.jo01Vehicles;
+
+public class Car extends Vehicle {
+    public final static double AC_ADDITIONAL_CONSUMPTION = 0.9;
+
+    public Car(double fuelQuantity, double fuelConsumption) {
+        super(fuelQuantity, fuelConsumption + AC_ADDITIONAL_CONSUMPTION);
+    }
+
+}
