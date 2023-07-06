caa1a87a232345b968a228dc9ecd11237677d6bc
Ivan Marinov <vando1661@gmail.com>
2023-07-06 14:14:20 +0100

    Delete Extensions.java

--- Java-OPP/_10PolymorphismExercises/calculator/Extensions.java
+++
@@ -1,7 +0,0 @@
-package Java_OOP._10PolymorphismExercises.calculator;
-
-public class Extensions {
-    public static InputInterpreter buildInterpreter(CalculationEngine engine) {
-        return  new InputInterpreter(engine);
-    }
-}
