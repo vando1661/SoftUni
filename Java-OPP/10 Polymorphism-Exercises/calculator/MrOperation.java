e08613d2ae112c840113fe8f05304fb7b0d001dc
Ivan Marinov <vando1661@gmail.com>
2023-07-06 14:13:47 +0100

    Create MrOperation.java

---
+++ Java-OPP/10 Polymorphism-Exercises/calculator/MrOperation.java
@@ -0,0 +1,25 @@
+package Java_OOP._10PolymorphismExercises.calculator;
+
+import java.util.Deque;
+
+public class MrOperation implements Operation{
+    private Deque<Integer> memory;
+    public MrOperation(Deque<Integer>memory){
+        this.memory = memory;
+    }
+
+    @Override
+    public void addOperand(int operand) {
+
+    }
+
+    @Override
+    public int getResult() {
+        return memory.pop();
+    }
+
+    @Override
+    public boolean isCompleted() {
+        return true;
+    }
+}
