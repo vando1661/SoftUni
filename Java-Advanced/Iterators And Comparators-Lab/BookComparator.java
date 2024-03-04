7f43a64a803b9d63310cbe4f4f9f9d16bb01a88d
Ivan Marinov <vando1661@gmail.com>
2024-03-04 12:54:11 +0000

    Create BookComparator.java

---
+++ Java-Advanced/Iterators And Comparators-Lab/BookComparator.java
@@ -0,0 +1,16 @@
+package Java_Advance._15IteratorsAndComparatorsLab;
+
+import java.util.Comparator;
+
+public class BookComparator implements Comparator<Book> {
+     @Override
+     public int compare(Book f, Book s){
+             int result = f.getTitle().compareTo(s.getTitle());
+
+             if (result == 0){
+                 result = Integer.compare(f.getYear(),s.getYear());
+             }
+             return result;
+
+     }
+}
