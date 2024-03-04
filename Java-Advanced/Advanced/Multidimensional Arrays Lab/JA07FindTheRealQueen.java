8fc06a4644f734c83a7b0e0bc12305eaed70d7c7
Ivan Marinov <vando1661@gmail.com>
2024-03-04 12:52:19 +0000

    Delete JA07FindTheRealQueen.java

--- Java-Advanced/Advanced/Multidimensional Arrays Lab/JA07FindTheRealQueen.java
+++
@@ -1,9 +0,0 @@
-package _3MultidimensionalArraysLab;
-
-import java.util.Scanner;
-
-public class JA07FindTheRealQueen {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner (System.in);
-    }
-}
