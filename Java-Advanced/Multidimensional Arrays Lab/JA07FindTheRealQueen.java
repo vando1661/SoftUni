77e061e4a52cd941b0ad3ace9673ed8f2f3517c4
Ivan Marinov <vando1661@gmail.com>
2024-03-04 12:54:27 +0000

    Create JA07FindTheRealQueen.java

---
+++ Java-Advanced/Multidimensional Arrays Lab/JA07FindTheRealQueen.java
@@ -0,0 +1,9 @@
+package _3MultidimensionalArraysLab;
+
+import java.util.Scanner;
+
+public class JA07FindTheRealQueen {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner (System.in);
+    }
+}
